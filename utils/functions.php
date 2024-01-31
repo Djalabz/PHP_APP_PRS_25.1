9c5830628beac4f2960ec471218dfb178c185060
djalabz <djalabz@gmail.com>
2024-01-31 14:42:31 +0100

    :bug: email password check fixed

--- utils/functions.php
+++ utils/functions.php
@@ -1,10 +1,17 @@
 <?php 
 
+// On va utiliser ce fichier pour regrouper les fonctions utiles de notre application
+
 function checkExists($field, $param, $pdo) {
     // Vérifier si une personne avec le meme nom ou le meme email n'existe pas déjà en BDD
     // Si c'est le cas on veut le préciser dans un message d'erreur
     $sql = "SELECT * FROM users WHERE $field = ?";
     $stmt = $pdo->prepare($sql);
     $result = $stmt->execute([$param]);
-    return $result;
-} 
+
+    if ($stmt->rowCount() > 0) {
+        return true; 
+    } else {
+        return false;
+    }
+}
--- views/signup.view.php
+++ views/signup.view.php
@@ -24,38 +24,48 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && $_POST['password'] === $_POST['conf
             die();
         }
 
-        // On vérifie que le mdp correspone aux critères de la CNIL (12 car, au moins 1 chiffre, 1 Maj, 1 min et 1 caractère spécial)
-        // Fonction pour vérifier une regex sur une string preg_match()
+        // Validation, selon les critères de la CNIL, du format du mot de passe
+        $regex = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{12,}$/";
+        $passValid = preg_match($regex, $password);
 
-        // Avant d'envoyer le mdp en bdd il faut le hasher !
-        $hash = password_hash($password, PASSWORD_DEFAULT);
+        var_dump($passValid);
 
-        // 
-        if (checkExists('name', $name, $pdo)) {
-            $error = "Le nom est déjà pris";
-        } else if (checkExists('email', $email, $pdo)) {
-            $error = "L'email est déjà pris";
-        } else {
-            // Écrire une requete préparée avec pdo
-            $sql = "INSERT INTO users(name, email, password) VALUES(?, ?, ?)";
-            $stmt = $pdo->prepare($sql);
-            $result = $stmt->execute([$name, $email, $hash]);
+        if ($passValid) {
+            // On vérifie que le mdp correspone aux critères de la CNIL (12 car, au moins 1 chiffre, 1 Maj, 1 min et 1 caractère spécial)
+            // Fonction pour vérifier une regex sur une string preg_match()
 
-            // Afficher les erreurs si il y en a, sinon un message de succès 
-            if ($result) {
-                // On redirige vers une page de succès 
-                header('Location: signup-sucess.view.php');
-                // On termine notre zone tampon
-                ob_end_flush();
+            // Avant d'envoyer le mdp en bdd il faut le hasher !
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            // Avec checkExists (importée depuis functions.php) on vérifie si le name ou email 
+            // n'existe pas déjà en BDD
+            if (checkExists('name', $name, $pdo)) {    
+                $error = "Le nom est déjà pris";
+            } else if (checkExists('email', $email, $pdo)) {
+                $error = "L'email est déjà pris";
             } else {
-                $error = "Erreur pendant l'ajout : " . $stmt->errorInfo();
-            } 
+                // Écrire une requete préparée avec pdo
+                $sql = "INSERT INTO users(name, email, password) VALUES(?, ?, ?)";
+                $stmt = $pdo->prepare($sql);
+                $result = $stmt->execute([$name, $email, $hash]);
+
+                // Afficher les erreurs si il y en a, sinon un message de succès 
+                if ($result) {
+                    // On redirige vers une page de succès 
+                    header('Location: signup-sucess.view.php');
+                    // On termine notre zone tampon
+                    ob_end_flush();
+                } else {
+                    $error = "Erreur pendant l'ajout : " . $stmt->errorInfo();
+                } 
+            }
+        } else {
+            $error = "Le format du mot de passe n'est pas le bon. Il doit faire 12 caractères au minimum et inclure une minuscule, une majuscule, un chiffre et un des caractères spéciaux suivants : #?!@$ %^&*-";
         }
     }
     else {
         $error = "Veuillez remplir tous les champs";
     }
-
 } else if ($_POST['password'] != $_POST['confirm']) {
     $error = "Les mots de passe doivent etre identiques";
 }
