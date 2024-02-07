2a296f90165c9e690649cf64b09e149367b20c50
djalabz <djalabz@gmail.com>
2024-02-07 12:59:03 +0100

    :lipstick: menu js file added

--- partials/header.php
+++ partials/header.php
@@ -2,8 +2,6 @@
 
 session_start();
 
-include "utils/functions.php";
-
 // Adresse de base qu'on transforme en tableau et on sépare avec les /
 // $array = explode('/', $url);
 
@@ -13,7 +11,15 @@ include "utils/functions.php";
 // On fait notre opérateur ternaire et on adapte la valeur de $path
 // $uri === 'index.php' ? $path = 'views/' : $path = '';
 
-$_SERVER['REQUEST_URI'] === '/index.php' ? $path = 'views/' : $path = '';
+$_SERVER['REQUEST_URI'] === '/index.php' ? $path = '' : $path = '..';
+
+if ($_SERVER['REQUEST_URI'] === '/index.php') {
+    $path = '';
+    $path_css = 'views/';
+} else {
+    $path = '..';
+    $path_css = '';
+}
 
 ?> 
 
@@ -23,32 +29,28 @@ $_SERVER['REQUEST_URI'] === '/index.php' ? $path = 'views/' : $path = '';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon eshop en PHP</title>
-    <link rel="stylesheet" href="<?= $path ?>style/style.css">
+    <link rel="stylesheet" href="<?= $path ?>/views/style/style.css">
+    <script src="<?= $path ?>/views/scripts/app.js" defer></script>
 </head>
 <body>
     <nav>
-        <ul>
+        <img class="burger-btn" src="<?= $path ?>/assets/icons/burger.png">
+        <ul class="menu">
             <li><a href="/index.php">Home</a></li>
-            <li><a href="<?= $path ?>contact.view.php">Contact</a></li>
+            <li><a href="<?= $path ?>/views/contact.view.php">Contact</a></li>
 
             <?php if (isset($_SESSION['user']) && $_SESSION['user']['logged']) :  ?> 
 
-                <li><a href="<?= $path ?>products.view.php">Products</a></li>
-                <li><a href="<?= $path ?>cart.view.php">Cart</a></li>
-                <li><a href="<?= $path ?>profile.view.php">Profile</a></li>
-                <li><a href="<?= $path ?>logout.php">Logout</a></li>
+                <li><a href="<?= $path ?>/views/products.view.php">Products</a></li>
+                <li><a href="<?= $path ?>/views/cart.view.php">Cart</a></li>
+                <li><a href="<?= $path ?>/views/profile.view.php">Profile</a></li>
+                <li><a href="<?= $path ?>/views/logout.php">Logout</a></li>
             
             <?php else : ?>
-                <li><a href="<?= $path ?>signup.view.php">Signup</a></li>
-                <li><a href="<?= $path ?>login.view.php">Login</a></li>
+                <li><a href="<?= $path ?>/views/signup.view.php">Signup</a></li>
+                <li><a href="<?= $path ?>/views/login.view.php">Login</a></li>
             <?php endif ?>
         </ul>
     </nav>
 <div class="wrapper">
 
-<?php 
-
-
-// dd($_SERVER);
-
-?>
