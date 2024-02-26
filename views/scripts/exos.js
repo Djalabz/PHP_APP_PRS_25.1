899ff49f4ca421b9c685ca84f7a7c3ccc744968c
djalabz <djalabz@gmail.com>
2024-02-26 14:22:29 +0100

    :sparkles: html code editor exercice added

--- views/scripts/exos.js
+++ views/scripts/exos.js
@@ -1,34 +1,18 @@
-import data from './data.js'
 
-const list = document.querySelector('.list')
+// 3) Exo Editeur HTML
 
-// Afficher chaque perso comme dans l'énoncé. 
-// On créera la plupart des éléments depuis JS 
-// On pourra faire un peu de CSS pour l'affichage en grid
+const input = document.querySelector('#input')
+const output = document.querySelector('.output')
+const btn = document.querySelector('button')
 
-data.forEach((item) => {
-    // Créer les éléments (h1, h2, img etc) dans notre document
-    const wrapper = document.createElement('div')
-    const name = document.createElement('h1')
-    const job = document.createElement('h2')
-    const img = document.createElement('img')
 
-    // Donner du contenu à ces éléments
-    name.textContent =  item.name
-    job.textContent = item.job
-    img.src = item.image
-
-    // Insérer ces éléments dans le wrapper (appendChild)
-    wrapper.appendChild(name);
-    wrapper.appendChild(job);
-    wrapper.appendChild(img);
-
-    // J'insère mon warpper dans la div list (cf le HTML)
-    list.appendChild(wrapper)
-
+btn.addEventListener('click', () => {
+    output.innerHTML = input.value
+    input.value = ""
 })
 
 
+// 1)  Exo Bouton d'incrémentation
 
 // const button = document.querySelector('button')
 // const output = document.querySelector('.output')
@@ -36,10 +20,41 @@ data.forEach((item) => {
 // let count = 0;
 
 // button.addEventListener('click', () => {
-//     if (count < 20) {
-//         count ++;
-//         output.textContent = count;
-//     } 
+    //     if (count < 20) {
+        //         count ++;
+        //         output.textContent = count;
+        //     } 
 // })
+        
+        
+        
+// 2)  Exo affichage des persos
 
+// import data from './data.js'
 
+// const list = document.querySelector('.list')
+
+// Afficher chaque perso comme dans l'énoncé. 
+// On créera la plupart des éléments depuis JS 
+// On pourra faire un peu de CSS pour l'affichage en grid
+
+// data.forEach((item) => {
+    // Créer les éléments (h1, h2, img etc) dans notre document
+    // const wrapper = document.createElement('div')
+    // const name = document.createElement('h1')
+    // const job = document.createElement('h2')
+    // const img = document.createElement('img')
+
+    // Donner du contenu à ces éléments
+    // name.textContent =  item.name
+    // job.textContent = item.job
+    // img.src = item.image
+
+    // Insérer ces éléments dans le wrapper (appendChild)
+    // wrapper.appendChild(name);
+    // wrapper.appendChild(job);
+    // wrapper.appendChild(img);
+
+    // J'insère mon warpper dans la div list (cf le HTML)
+//     list.appendChild(wrapper)
+// })
